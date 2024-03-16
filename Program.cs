7da7d60972835127fffabc0363e361375cbf6993
ETEC Adolpho Berezin <meu-email@meu-servidor.com>
2024-03-15 22:10:40 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// Console.WriteLine("joao pedro".Replace("j" , "ppp"));
+
+
+ Console.Clear();
+
+ Console.WriteLine("tradutor de baleês \n");
+ Console.Write("Digite o texto a ser traduzido: ");
+
+ string texto = Console.ReadLine()!;
+
+ Console.WriteLine("\nA dory diria...\n");
+
+ Console.WriteLine(texto
+ .Replace("a" , "aaa")
+ .Replace("i" , "iii")
+ .Replace("u" , "uuu")
+ .Replace("e" , "eee")
+ .Replace("o" , "ooo")
+ .Replace("a" , "aaa")
+ .Replace("i" , "iii")
+ .Replace("u" , "uuu")
+ .Replace("e" , "eee")
+ .Replace("o" , "ooo")
+ );
+ Console.WriteLine();
